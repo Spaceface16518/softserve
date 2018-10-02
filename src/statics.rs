e8589dd7ee82bffcfd529468410c9997e62b89d6
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-10-02 18:19:00 -0500

    Add initial statics

---
+++ src/statics.rs
@@ -0,0 +1,13 @@
+pub const PORT: &str = "127.0.0.1:8080";
+
+pub const DEF_RESPONSE: &str = "<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <meta charset=\"utf-8\">
+    <title>Hello!</title>
+  </head>
+  <body>
+    <h1>Hello!</h1>
+    <p>Hi from Rust</p>
+  </body>
+</html>";
