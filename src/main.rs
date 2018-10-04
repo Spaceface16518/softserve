fd85ea1a1588180d5dce80039974caa7ba93b736
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-10-04 17:45:33 -0500

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io::prelude::*;
+use std::path::Path;
 use std::net::{TcpListener, TcpStream};
+use std::fs::read_to_string;
 
 mod statics;
 mod thread;
@@ -24,14 +26,22 @@ fn handle(mut stream: TcpStream) {
     let mut buffer: [u8; 512] = [0; 512];
     stream.read(&mut buffer).unwrap();
 
+    // TODO: refactor this and include file loading, default response, and error404 support
+
     let (status_line, data) = if buffer.starts_with(statics::GET_PREFIX.as_bytes()) {
-        (statics::STATUS_LINE_200, statics::DEF_RESPONSE)
+        (statics::STATUS_LINE_200, "test_data/file.txt")
     } else {
-        (statics::STATUS_LINE_404, statics::ERROR_404_RESPONSE)
+        (statics::STATUS_LINE_404, "test_data/error404.html")
     };
 
-    let response = format!("{}{}", status_line, data);
+    let response = format!("{}{}", status_line, read_to_string(data).unwrap().as_str());
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+enum BufCheck {
+    File(String),
+    Def,
+    Error404,
 }
