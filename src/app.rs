edb8f494682666914f50fabd63b29fca9464ac53
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-10-31 18:41:53 -0500

    Replace macro system with direct function

--- src/app.rs
+++ src/app.rs
@@ -2,71 +2,32 @@ extern crate clap;
 
 use self::clap::{App, Arg};
 
-#[macro_export]
-macro_rules! app {
-    ($params:expr) => {
-        App::new($params.name())
-            .version($params.version())
-            .author($params.authors())
-            .about($params.description())
-            // MARK: arg port
-            .arg(
-                Arg::with_name("port")
-                    .short("p")
-                    .long("port")
-                    .takes_value(true)
-                    .required(true)
-                    .help("Sets the port to listen at"),
-            )
-            // MARK: arg path
-            .arg(
-                Arg::with_name("path")
-                    .short("s")
-                    .long("path")
-                    .takes_value(true)
-                    .required(true)
-                    .help("Sets the path of the public asset folder"),
-            )
-    };
-}
-
-type param_inner<'a> = &'a str;
-
-pub struct AppParams<'a> {
-    name: param_inner<'a>,
-    version: param_inner<'a>,
-    author: param_inner<'a>,
-    description: param_inner<'a>,
-}
-
-impl<'a> AppParams<'a> {
-    pub fn new(
-        name: param_inner<'a>,
-        version: param_inner<'a>,
-        author: param_inner<'a>,
-        description: param_inner<'a>,
-    ) -> AppParams<'a> {
-        AppParams {
-            name,
-            version,
-            author,
-            description,
-        }
-    }
-
-    pub fn name(&self) -> param_inner {
-        self.name
-    }
-
-    pub fn version(&self) -> param_inner {
-        self.version
-    }
-
-    pub fn author(&self) -> param_inner {
-        self.author
-    }
-
-    pub fn description(&self) -> param_inner {
-        self.description
-    }
+pub fn app<'a, 'b>(
+    name: &'b str,
+    version: &'b str,
+    author: &'b str,
+    description: &'b str,
+) -> App<'a, 'b> {
+    App::new(name)
+        .version(version)
+        .author(author)
+        .about(description)
+        // MARK: arg port
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .takes_value(true)
+                .required(true)
+                .help("Sets the port to listen at"),
+        )
+        // MARK: arg path
+        .arg(
+            Arg::with_name("path")
+                .short("s")
+                .long("path")
+                .takes_value(true)
+                .required(true)
+                .help("Sets the path of the public asset folder"),
+        )
 }
